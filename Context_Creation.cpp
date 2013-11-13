f12143ca9079f08b958c1c08b3aac1d5d708411f
Luka Horvat <redreaper132@gmail.com>
2013-11-13 21:36:25 +0100

    GLFW initi code

--- Context_Creation.cpp
+++ Context_Creation.cpp
@@ -1,4 +1,38 @@
+//Libs
+#ifdef _WIN32
+#pragma comment(lib, "glfw3.lib")
+#pragma comment(lib, "opengl32.lib")
+#endif // _WIN32
+
+//Includes
+#include <GLFW/glfw3.h>
+#include <thread>
+
 int main(int argc, char* args[])
 {
+    //Init stuff
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", nullptr, nullptr); // Windowed
+
+    glfwMakeContextCurrent(window);
+
+    //Loopideeloop
+    while (!glfwWindowShouldClose(window))
+    {
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        {
+            glfwSetWindowShouldClose(window, true);
+        }
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
     return 0;
 }
