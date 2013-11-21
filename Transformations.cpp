8e4c84079d315cabc78645f6bb382d7bcb855a45
Luka Horvat <redreaper132@gmail.com>
2013-11-21 19:57:27 +0100

    Wrote base for context creation

--- Transformations.cpp
+++ Transformations.cpp
@@ -1,4 +1,84 @@
+//libs
+#ifdef _WIN32
+#pragma comment(lib, "glew32s.lib")
+#pragma comment(lib, "glfw3.lib")
+#pragma comment(lib, "opengl32.lib")
+#pragma comment(lib, "SOIL.lib")
+#endif
+
+//Includes
+#ifdef _WIN32
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <SOIL/SOIL.h>
+#include <thread>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#endif
+
+//Messy unoptimized function to compile a shader and outputing errors
+GLuint compileShader(const std::string file_name, int shader_type, bool print_error = true);
+
 int main(int argc, char* args[])
 {
+    //glfw stuff
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, false);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Transformations", nullptr, nullptr);
+    glClearColor(1.f, 1.f, 1.f, 1.f);
+
+    //glew stuff
+    glewExperimental = true;
+    glewInit();
+
+    while(!glfwWindowShouldClose(window))
+    {
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        {
+            glfwSetWindowShouldClose(window, true);
+        }
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
     return 0;
+}
+
+GLuint compileShader(const std::string file_name, int shader_type, bool print_error)
+{
+    GLuint shader = glCreateShader(shader_type); //Create shader
+    std::ifstream shader_file(file_name); //Read file
+    std::stringstream file_stream;
+    if (shader_file)
+    {
+        file_stream << shader_file.rdbuf();
+    }
+    else
+    {
+        std::cout << "Reading '" + file_name + "' faled!\n";
+        return 0;
+    }
+    shader_file.close();
+    std::string content = file_stream.str();
+    const char* source = content.c_str();
+    glShaderSource(shader, 1, &source, NULL); //Copy shader to card
+    glCompileShader(shader); //Compile
+    int status = 0;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+    if (status == false)
+    {
+        char buffer[512];
+        glGetShaderInfoLog(shader, 512, NULL, buffer);
+        std::cout << "Compiling '" + file_name + "' failed:\n" << buffer << "\n";
+        return 0;
+    }
+    return shader;
 }
