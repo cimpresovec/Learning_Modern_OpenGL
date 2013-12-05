97f92230b2af4ddf3e44832914a5866017deeb2c
Luka Horvat <redreaper132@gmail.com>
2013-12-05 10:35:32 +0100

    Testing moving to a single VBO
    Much much better performace

--- Moving_Around.cpp
+++ Moving_Around.cpp
@@ -35,7 +35,7 @@ struct Player
 
     void handleInput(GLFWwindow* main_window);
     void doLogic();
-    void render();
+    void render(float* bigo);
 
 private:
     GLuint vbo;
@@ -76,7 +76,35 @@ int main(int argc, char* args[])
     Player* player_one = new Player(shader_program);
     Player* player_two = new Player(shader_program);
 
-    int count = 0;
+    //Benchmark
+    const int MAX = 5000;
+    Player* players[MAX];
+    for (int n = 0; n < MAX; ++n)
+    {
+        players[n] = new Player(shader_program);
+    }
+
+    float vertices[MAX][24];
+    GLuint vbo;
+
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
+
+    //vao
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    //Attributes
+    GLint position_attribute = glGetAttribLocation(shader_program, "in_position");
+    glEnableVertexAttribArray(position_attribute);
+    glVertexAttribPointer(position_attribute, 2, GL_FLOAT, 0, 6*sizeof(float), 0);
+
+    GLint color_attribute = glGetAttribLocation(shader_program,"in_color");
+    glEnableVertexAttribArray(color_attribute);
+    glVertexAttribPointer(color_attribute, 4, GL_FLOAT, 0, 6*sizeof(float), (void*)(2*sizeof(float)));
+    //glBindVertexArray(0);
 
     //Main game loop
     while (!glfwWindowShouldClose(main_window))
@@ -90,30 +118,62 @@ int main(int argc, char* args[])
             glfwSetWindowShouldClose(main_window, true);
         }
         player_one->handleInput(main_window);
+        for (int n = 0; n < MAX; ++n)
+        {
+            players[n]->handleInput(main_window);
+        }
 
         //LOGIC
         player_one->doLogic();
+        for (int n = 0; n < MAX; ++n)
+        {
+            players[n]->doLogic();
+        }
 
         //RENDERING
         glClear(GL_COLOR_BUFFER_BIT);
 
-        
         //Render
-        player_one->render();
-        player_two->render();
+        //player_one->render();
+        //player_two->render();
+        for (int n = 0; n < MAX; ++n)
+        {
+            players[n]->render(vertices[n]);
+            //verticec[n] = players[n]->render();
+        }
+
+       /* for (int n = 0; n < MAX; ++n)
+        {
+            for (int g = 0; g < 24; ++g)
+            {
+                std::cout << vertices[n][g] << " ";
+            }
+            std::cout << std::endl;
+        }*/
+        
+
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferData(GL_ARRAY_BUFFER, 0, 0, NULL);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
+
+        //glBindVertexArray(vao);
+        //glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+        //glBindVertexArray(0);
+        for (int n = 0; n < MAX; ++n)
+        {
+            glDrawArrays(GL_TRIANGLE_FAN, n*24, 4);
+        }
 
         glfwSwapBuffers(main_window);
 
         //timer fps
         std::chrono::high_resolution_clock::time_point end_time = std::chrono::high_resolution_clock::now();
-        std::chrono::microseconds frame_time = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
-        if (frame_time.count() < 16000)
+        std::chrono::milliseconds frame_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+        if (frame_time.count() < 16)
         {
             //std::cout << "Delayu " << frame_time.count() << std::endl;
-            std::this_thread::sleep_for(std::chrono::microseconds(16000 - frame_time.count())); //Delay
+            std::this_thread::sleep_for(std::chrono::milliseconds(16 - frame_time.count())); //Delay
         }
-        count = (count+1) % 60;
-        std::cout << count << "\n";
         
     }
 
@@ -137,7 +197,7 @@ Player::Player(const int shader)
     
     glGenBuffers(1, &vbo);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STREAM_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
     
     //vao
     glGenVertexArrays(1, &vao);
@@ -171,46 +231,49 @@ void Player::doLogic()
     glm::vec2 speed(0.f, 0.f);
     if (right_button)
     {
-        speed.x += 0.001f;
+        speed.x += 0.01f;
     }
     if (left_button)
     {
-        speed.x -= 0.001f;
+        speed.x -= 0.01f;
     }
     if (up_button)
     {
-        speed.y += 0.001f;
+        speed.y += 0.01f;
     }
     if (down_button)
     {
-        speed.y -= 0.001f;
+        speed.y -= 0.01f;
     }
 
     x += speed.x;
     y += speed.y;
 }  
 
-void Player::render()
+void Player::render(float* bigo)
 {
     //vbo
     //update vbo
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, 0, 0, NULL);
+    //glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    //glBufferData(GL_ARRAY_BUFFER, 0, 0, NULL);
     
-    float vertices[] = 
+    //float* vertices = new float[24];
+    float vertices2[] = 
     {
         x-w/2, y-h/2, 1.f, 1.f, 1.f, .5f,
         x+w/2, y-h/2, 0.f, 0.f, 1.f, .5f,
         x+w/2, y+h/2, 1.f, 0.f, 0.f, .5f,
         x-w/2, y+h/2, 0.f, 1.f, 0.f, .5f
     };
+    memcpy(bigo, vertices2, sizeof(vertices2));
 
-    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
+
+    //glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
 
     //Render
-    glBindVertexArray(vao);
-    glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
-    glBindVertexArray(0);
+    //glBindVertexArray(vao);
+    //glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+   // glBindVertexArray(0);
 }
 
 GLuint compileShader(const std::string file_name, int shader_type, bool print_error)
